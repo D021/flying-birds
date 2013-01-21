37559b98c2e3f5cfc1b1791a34cd755acc798dbb
Rodrigo Davies <rodrigo.davies@gmail.com>
2013-01-20 19:47:37 -0500

    Initial commit
    jquery mobile, jquery, bcrypt and bootstrap gems

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FlyingBirds::Application.config.secret_token = '8b5a201b56766f5fe74845d8480c4db462edee10f1b5b93576975da78053ca92a8a9387e8e4c2e9cad22e8cda5d42e33f5af5be258ca45cb2375b5e66b914b43'
